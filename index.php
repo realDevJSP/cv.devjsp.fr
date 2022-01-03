0e835200adbe97bb87a5c036d93fa8e72d80892e
root <root@devjsp.fr>
2022-01-03 02:56:25 +0100

    project init

---
+++ index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html>
+<title>Julien Salgueiro Pereira</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="/assets/logo.ico" />
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
+<style>
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+</style>
+<body class="w3-light-grey">
+
+<!-- Page Container -->
+<div class="w3-content w3-margin-top" style="max-width:1400px;">
+
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+  
+    <!-- Left Column -->
+    <div class="w3-third" style="float: fixed;">
+    
+      <div class="w3-white w3-text-grey w3-card-4">
+        <div class="w3-display-container">
+          <img src="/assets/profil.jpg" style="width:100%" alt="Avatar">
+          <div class="w3-display-bottomleft w3-white w3-container w3-text-black">
+            <h2>Julien Salgueiro Pereira</h2>
+          </div>
+        </div>
+        <div class="w3-container">
+          <p><i class="fa fa-briefcase fa-fw w3-margin-right w3-large" style="color:#0061FF"></i>Développeur</p>
+          <p><i class="fa fa-home fa-fw w3-margin-right w3-large" style="color:#0061FF"></i>Creutzwald, Moselle, France</p>
+          <p><i class="fa fa-envelope fa-fw w3-margin-right w3-large" style="color:#0061FF"></i><a href="mailto:contact@devjsp.fr">contact@devjsp.fr</a></p>
+          <p><i class="fa fa-phone fa-fw w3-margin-right w3-large" style="color:#0061FF"></i>06 32 24 28 10</p>
+          <hr>
+
+          <p class="w3-large"><b><i class="fa fa-asterisk fa-fw w3-margin-right" style="color:#0061FF"></i>Compétences</b></p>
+          <p>Administration système Linux</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-container w3-center w3-round-xlarge" style="width:60%; background-color:#369FFF; color:#F4F4F4;">60%</div>
+          </div>
+          <p>HTML/CSS</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-container w3-center w3-round-xlarge" style="width:80%; background-color:#369FFF; color:#F4F4F4;">80%</div>
+          </div>
+          <p>PHP</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-container w3-center w3-round-xlarge" style="width:75%; background-color:#369FFF; color:#F4F4F4;">75%</div>
+          </div>
+          <p>Java</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-container w3-center w3-round-xlarge" style="width:70%; background-color:#369FFF; color:#F4F4F4;">70%</div>
+          </div>
+          <p>W-Langages</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-container w3-center w3-round-xlarge" style="width:90%; background-color:#369FFF; color:#F4F4F4;">90%</div>
+          </div>
+          <p>SQL / Administration BDD</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-container w3-center w3-round-xlarge" style="width:90%; background-color:#369FFF; color:#F4F4F4;">90%</div>
+          </div>
+          <br>
+          <hr>
+
+          <p class="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right" style="color:#0061FF"></i>Langues</b></p>
+          <p>Anglais</p>
+          <div class="w3-light-grey w3-round-xlarge">
+            <div class="w3-container w3-center w3-round-xlarge" style="width:95%; background-color:#369FFF; color:#F4F4F4;">95%</div>
+          </div>
+          <br>
+          <hr>
+
+          <p class="w3-large w3-text-theme"><b><i class="fa fa-search fa-fw w3-margin-right" style="color:#0061FF"></i>Informations complémentaires</b></p>
+          <p>Célibataire</p>
+          <p>Titulaire d'un permis B et propriétaire d'un véhicule</p>
+          <p>Passionné d'informatique et de nouvelles technologies</p>
+          <p>Musique, cinéma, voyages</p>
+          <p>Veni, vidi, vici</p>
+        </div>
+      </div><br>
+
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Right Column -->
+    <div class="w3-twothird">
+    
+     <div class="w3-container w3-card w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge" style="color:#0061FF"></i>Etudes</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>DUT Informatique</b></h5>
+          <h6 style="color:#0061FF"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2013 - 2015</h6>
+          <p>IUT de Saint-Dié-des-Vosges</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Baccalauréat Scientifique</b></h5>
+          <h6 style="color:#0061FF"><i class="fa fa-calendar fa-fw w3-margin-right"></i>2010 - 2013</h6>
+          <p>Lycée Polyvalent Félix Mayer de Creutzwald</p><br>
+        </div>
+      </div>
+
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge" style="color:#0061FF"></i>Expériences professionnelles</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Télé-conseiller / 1&1 IONOS</b></h5>
+          <h6 style="color:#0061FF"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Mai 2021 - <span class="w3-tag w3-round" style="background-color:#0061FF">Aujourd'hui</span></h6>
+          <p>Au sein de l'équipe française de conseillers Serveur de la société 1&1 IONOS, mon travail consite à répondre aux questions et besoins techniques des clients de la société. Cela m'a permis de grandement améliorer mes capacités de communication et d'acquérir des compétences en administration système Linux et les connaissances qui me manquait en terme d'hébergement de sites internet.</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Dévelopeur W-Langages / LAVANCE</b></h5>
+          <h6 style="color:#0061FF"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Août 2018 - Novembre 2020</h6>
+          <p>En tant que développeur principal de l'équipe M2M chez LAVANCE, j'ai eu la chance de participer à chaques étapes de plusieurs projets informatiques : de l'analyse du besoin à la conception de la solution et du développement du produit jusqu'à sa maintenance, évolutive ou non. La plupart de ces projets étant développés en W-Langages (Windev et Webdev), j'ai aussi eu l'occasion de travailler sur des projets PHP et JavaScript.</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Développeur C et 4D / EDITSOFT</b></h5>
+          <h6 style="color:#0061FF"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Mai 2015 - Août 2015</h6>
+          <p>Pendant ce stage de fin d'études lors de mon DUT, j'ai eu l'opportunité de développer, en plus du C, dans un langage peu répendu : le 4D. Le projet était de pouvoir remplir un formulaire administratif 4D en placant une carte vitale sur un lecteur de carte à puce. J'ai donc réalisé un petit programme en C, exécuté par un formulaire 4D dès la détection d'une carte à puce, remplissant automatiquement les différents champs du formulaire avec les différentes informations de la Carte Vitale.</p><br>
+        </div>
+      </div>
+
+      <div class="w3-container w3-card w3-white">
+        <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-flask fa-fw w3-margin-right w3-xxlarge" style="color:#0061FF"></i>Projets personnels</h2>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Administration système Linux et hébergement de solutions Web</b></h5>
+          <p>Travailler chez 1&1 IONOS m'a permis d'acquérir des connaissances de bases d'administration de systèmes Linux et des compétences avancées dans la configuration de serveurs Web. Je suis aujourd'hui administrateur de mon propre serveur virtuel me permettant d'héberger ce même site internet et d'autre solutions Web développées par mes soins.</p>
+          <hr>
+        </div>
+        <div class="w3-container">
+          <h5 class="w3-opacity"><b>Entreprenariat</b></h5>
+          <p>Ayant récemment adopté le statut d'auto-entrepreneur, je propose la création de solutions informatiques à des proches et connaissances, me chargeant personnellement de l'analyse, la conception, le développement et la maintenance du produit. Depuis cette fin d'année 2021, en plus de ce CV, un <a href="https://portfolio.devjsp.fr" target="_blank">Portfolio</a> personnel, un <a href="https://blog.devjsp.fr" target="_blank">Blog</a> et un <a href="https://devjsp.fr" target="_blank">Site vitrine</a> sont disponibles sur internet afin de promouvoir mon activité.</p>
+        </div>
+      </div>
+
+    <!-- End Right Column -->
+    </div>
+    
+  <!-- End Grid -->
+  </div>
+  
+  <!-- End Page Container -->
+</div>
+
+<footer class="w3-container w3-center w3-margin-top w3-padding-top" style="background-color:#0061FF; color:#F4F4F4;">
+  <p><a class="w3-text-white w3-margin-top" href="downloads/cv.jpg" downlaod><i class="fa fa-download fa-fw w3-margin-right w3-text-white"></i></a></p>
+  <p>Celui qu'il vous faut.</p>
+</footer>
+
+</body>
+</html>
+
